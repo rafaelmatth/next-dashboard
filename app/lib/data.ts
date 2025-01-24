5e24479447533f6719aceddd8db0108baa378fed
Rafael <rafaelmat142@gmail.com>
2025-01-24 19:57:53 -0300

    feat: fetching data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -31,11 +31,11 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   try {
     const data = await sql<LatestInvoiceRaw>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
+      SELECT invoices.amount, customers.name, customers.image_url, customers.email
       FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
+             JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
-      LIMIT 5`;
+        LIMIT 5`;
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
